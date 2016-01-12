e93730cfd7eadee4c1cd19b90f042fa2cfd8a74e
Roman Resenchuk <romacv@bk.ru>
2016-01-12 18:04:48 +0300

    Update NSManagedObjectContext+RRHelpers.h

--- NSManagedObjectContext+RRHelpers.h
+++ NSManagedObjectContext+RRHelpers.h
@@ -1,14 +1,4 @@
-//
-//  NSManagedObjectContext+Helpers.h
-//  CookerDemo
-//
-//  Created by Dan on 16.06.15.
-//  Copyright (c) 2015 Алексей. All rights reserved.
-//
-
 #import <CoreData/CoreData.h>
-
-
 
 @interface NSManagedObjectContext (Helpers)
 
